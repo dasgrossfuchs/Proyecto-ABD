ad3bb394dfa9d7a55777db711481413577c9c177
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2020-04-15 23:01:02 -0700

    Se arreglaron problemas de librerias
    Referencias a conector de mysql no sirve en .net core, o no se como agregarlo :v USAR NET FRAMEWORK

---
+++ FrameworkABDConsola/FrameworkABDConsola/Interprete.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkABDConsola
+{
+    class Interprete
+    {
+        private string Com;
+        public void inicio(string comando)
+        {
+            string[] palabras = comando.Split(' ');
+            switch (palabras[0].ToLower())
+            {
+                case "salvar":
+                    switch (palabras[1].ToLower())
+                    {
+                        case "base":
+                            if (palabras.Length == 4)
+                            {
+                                //SALVAR BASE A ARCHIVO
+                                string archivo = palabras[3];
+
+                                //backup de todas las bases de datos al archivo
+                            }
+                            else
+                            {
+                                //SALVAR BASE NOMBRE,NOMBRE,NOMBRE A ARCHIVO
+                                string[] nombres = palabras[2].Split(',');
+                                string archivo = palabras[4];
+
+                                //backup de todas las bases de datos que conforman a nombres al archivo
+                            }
+                            break;
+                        case "tabla":
+                            if (palabras.Length == 5)
+                            {
+                                //SALVAR TABLA DBNAME A ARCHIVO
+                                string dbname = palabras[2];
+                                string archivo = palabras[3];
+
+                                //backup de todas las tablas de la base de datos nombrada dbname al archivo
+                            }
+                            else
+                            {
+                                //SALVAR TABLA DBNAME TABLA,TABLA,TABLA A ARCHIVO
+                                string dbname = palabras[2];
+                                string[] tablas = palabras[3].Split(',');
+                                string archivo = palabras[5];
+                                foreach (var item in tablas)
+                                {
+                                    //backup de las tablas nombradas en la base de datos dbname al archivo
+                                }
+                            }
+
+                            break;
+                        default:
+                            Console.WriteLine("Error procesando comando en:\'" + palabras[1].ToUpper() + "\'");
+                            break;
+                    }
+                    break;
+                case "recuperar":
+                    switch (palabras[1].ToLower())
+                    {
+                        case "base":
+                            if (palabras.Length == 4)
+                            {
+                                //RECUPERAR BASE DE ARCHIVO
+                                string archivo = palabras[3];
+
+                                //recuperacion de todas las bases de datos desde archivo
+                            }
+                            else
+                            {
+                                //RECUPERAR BASE NOMBRE,NOMBRE,NOMBRE DE ARCHIVO
+                                string[] nombres = palabras[2].Split(',');
+                                string archivo = palabras[4];
+
+                                //recuperacion de todas las bases de datos que conforman a nombres desde archivo
+                            }
+                            break;
+                        case "tabla":
+                            if (palabras.Length == 5)
+                            {
+                                //RECUPERAR TABLA DBNAME DE ARCHIVO
+                                string dbname = palabras[2];
+                                string archivo = palabras[3];
+
+                                //recuperacion de todas las tablas de la base de datos nombrada dbname desde archivo
+                            }
+                            else
+                            {
+                                //RECUPERAR TABLA DBNAME TABLA,TABLA,TABLA DE ARCHIVO
+                                string dbname = palabras[2];
+                                string[] tablas = palabras[3].Split(',');
+                                string archivo = palabras[5];
+                                foreach (var item in tablas)
+                                {
+                                    //recuperacion de las tablas nombradas en la base de datos dbname desde archivo
+                                }
+                            }
+
+                            break;
+                        default:
+                            Console.WriteLine("Error procesando comando en:\'" + palabras[1].ToUpper() + "\'");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error procesando comando en:\'" + palabras[0].ToUpper() + "\'");
+                    break;
+            }
+        }
+    }
+}
