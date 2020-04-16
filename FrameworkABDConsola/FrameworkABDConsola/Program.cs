450309c3ad2d713e524769c6983d3225b466190d
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2020-04-16 16:09:59 -0700

    yes

--- FrameworkABDConsola/FrameworkABDConsola/Program.cs
+++ FrameworkABDConsola/FrameworkABDConsola/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql;
+using MySql.Data.MySqlClient;
 
 namespace FrameworkABDConsola
 {
@@ -11,6 +11,27 @@ namespace FrameworkABDConsola
     {
         static void Main(string[] args)
         {
+            MySqlConnection conn;
+            Console.WriteLine("servidor : LOCALHOST");
+            Console.Write("Id usuario : ");
+            string uid = Console.ReadLine();
+            Console.Write("contraseña : ");
+            string pwd = Console.ReadLine();
+            string constr = "server=localhost; uid="+uid+";pwd="+pwd+";database=sakila";
+            try
+            {
+                conn = new MySqlConnection(constr);
+                conn.Open();
+                Console.WriteLine("Conexión exitosa!");
+                mysqlback
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            
+            
         }
     }
 }
